2877eaab1886e2b3b140ddc23c96e6e7b6de93f1
SebastianKorytkowski <sebastian.korytkowski@student.wat.edu.pl>
2022-01-11 03:31:32 +0100

    Start

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import datetime
 import json
 import requests
 import time
+import psutil
 
 settings = json.load(open('data.json'))
 address = "http://" + settings["address"];
@@ -9,7 +10,13 @@ id = settings['id']
 identityProof = settings['identityProof']
 
 while True:
-    data = {"timestamp": datetime.datetime.now().isoformat()}
+    cpu = psutil.cpu_percent()
+    ram = psutil.virtual_memory().percent
+
+    data = {
+        "cpuUsage": cpu,
+        "ramUsage": ram
+    }
     r = requests.post(address + "/logs/machine/" + id, {"jsonData": json.dumps(data), "identityProof":identityProof})
     print(r.content)
     time.sleep(1.0)
