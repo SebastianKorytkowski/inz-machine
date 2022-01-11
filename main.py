9f88d550dc5887b5b400455b16ae8bfd844ac0c9
SebastianKorytkowski <sebastian.korytkowski@student.wat.edu.pl>
2022-01-11 02:18:53 +0100

    Start

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import requests
 import time
 
 settings = json.load(open('data.json'))
-
+address = "http://" + settings["address"];
+id = settings['id']
+identityProof = settings['identityProof']
 
 while True:
     data = {"timestamp": datetime.datetime.now().isoformat()}
-    r = requests.post(settings['address'] + "/logs/machine/" + settings['id'], {"jsonData": json.dumps(data)})
+    r = requests.post(address + "/logs/machine/" + id, {"jsonData": json.dumps(data), "identityProof":identityProof})
     print(r.content)
     time.sleep(1.0)
-
